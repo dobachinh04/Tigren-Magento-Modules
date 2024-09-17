78a3b6c1d062a6c9e30a5c7fff02c555c8e1cec5
dobachinh04 <dobachinh04@gmail.com>
2024-09-17 17:28:15 +0700

    Diplay Discount List

---
+++ Tigren/CustomerGroupCatalog/Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tigren\CustomerGroupCatalog\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+    public function __construct(
+        private PageFactory $pageFactory
+    ) {
+
+    }
+
+    public function execute(): ResultInterface
+    {
+        $page = $this->pageFactory->create();
+        $page->getConfig()->getTitle()->set(__('Discount'));
+
+        return $page;
+    }
+}
